80aa69f0d5c8176204ed00f9fa49d9187ac42244
Roger-luo <hiroger@qq.com>
2018-10-22 01:38:27 -0400

    specialize hcat/vcat

--- src/operators/cat.jl
+++ src/operators/cat.jl
@@ -5,6 +5,15 @@ Base.cat(X::NA...; dims) = Base._cat(dims, X...)
 # Then define the method that catches at least one Value:
 Base.cat(X::NAR...; dims) = register(Base.cat, X...; dims=dims)
 
+Base.vcat(A::AbstractNode) = cat(A; dims=Val(1))
+Base.vcat(A::AbstractNode, B::AbstractArray) = cat(A, B; dims=Val(1))
+Base.vcat(A::AbstractArray, B::AbstractNode) = cat(A, B; dims=Val(1))
+Base.vcat(A::NAR...) = cat(A...; dims=Val(1))
+Base.hcat(A::AbstractNode) = cat(A; dims=Val(2))
+Base.hcat(A::AbstractNode, B::AbstractArray) = cat(A, B; dims=Val(2))
+Base.hcat(A::AbstractArray, B::AbstractNode) = cat(A, B; dims=Val(2))
+Base.hcat(A::NAR...) = cat(A...; dims=Val(2))
+
 gradient(::typeof(Base.cat), grad, output, As...; dims) = uncat(dims, grad, As...)
 
 uncat(dims, cat_output, xs...) = _uncat(Base.dims2cat(dims), cat_output, xs...)
